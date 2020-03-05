1351e128dd67a4b8896d7d629b8ca6651cc5c60a
Arizona <geekwid@gmail.com>
2020-03-05 15:59:19 +0100

    init depot

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index()
+    {
+        return $this->render('/index.html.twig');
+    }
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login()
+    {
+        return $this->render('/login.html.twig');
+    }
+
+    // Product controller
+    /**
+     * @Route("/getproduct", name="productDetail")
+     */
+    public function listProducts(int $id){
+        $products = $this->getDoctrine()
+        ->getRepository(Product::class)
+        ->findAll([], null, 50);
+
+        shuffle($product);
+        $products = array_slice($products, 0, 25);
+
+        $response = new Response();
+        $response->setContent(json_encode($products));
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
+    }
+}
