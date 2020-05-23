e43dc754d95df5b378bbff32984742eb904f5652
Arizona <geekwid@gmail.com>
2020-05-23 14:43:05 +0200

    feat: adding or removing qty in the cart with ajax

--- src/Service/Cart/CartService.php
+++ src/Service/Cart/CartService.php
@@ -28,12 +28,23 @@ class CartService {
         $this->session->set('panier', $panier);
     }
 
+    public function minus(int $id) {
+        $panier = $this->session->get('panier', []);
+
+        if(!empty($panier[$id])){
+            $panier[$id]--;
+        }
+
+        $this->session->set('panier', $panier);
+    }
+
     public function remove(int $id) {
         $panier = $this->session->get('panier', []);
 
         if(!empty($panier[$id])){
             unset($panier[$id]);
         }
+
         $this->session->set('panier', $panier);
     }
 
